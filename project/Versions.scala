b8fa2f82b2e5cc83e62678893ee45d5a12f7a1b5
aleastChs <aleast@student.chalmers.se>
2017-07-20 11:39:47 +0200

    Comments in project/Versions.scala

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,12 @@
 object Versions extends ScalaJS {
   val scalaPrimary = "2.12.2"
-  val scalaOlder = Seq("2.12.1", "2.11.8") // todo add more
+  val scalaOlder = Seq("2.12.1", "2.11.8")  // todo add more
 }
 
 trait ScalaJS {
+  // this is the current version of facades
+  // bintray do not use SNAPSHOT
+  // todo on update:
+  //                What do with version? Must change on every publish?
   val scalaJSGoogleCharts = "0.1.1"
 }
