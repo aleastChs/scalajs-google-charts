ba3792f1bf4f3aa760b020ee299bd2f6bf7ec86d
aleastChs <aleast@student.chalmers.se>
2017-08-01 15:42:29 +0200

    Changed Accsess name, Updated Timeline and DataTable

--- src/main/scala/org/aleastChs/clientlib/GoogleCharts.scala
+++ src/main/scala/org/aleastChs/clientlib/GoogleCharts.scala
@@ -6,126 +6,176 @@ import scala.scalajs.js
 import js.annotation.{JSName, JSGlobal}
 
 
-package google {
+package aleastchs {
 
-  @JSGlobal("google.charts")
-  @js.native
-  object charts extends js.Object {
-    def load(): Unit = js.native
+  package facade {
 
-    def setOnLoadCallback(): Unit = js.native
-  }
+    package google {
 
-  @JSGlobal("google.QueryResponse")
-  @js.native
-  class QueryResponse extends js.Object {
+      @JSGlobal("aleastchs.facade.google.charts")
+      @js.native
+      object charts extends js.Object {
+        def load(): Unit = js.native
 
-  }
+        def setOnLoadCallback(): Unit = js.native
+      }
 
-  @JSGlobal("google.visualization")
-  @js.native
-  object visualization extends js.Object{
-    // STATIC METHODS
-    def arrayToDataTable(twoDArray: js.Array[js.Array[js.Any]], opt_firstRowIsData: Boolean = false): DataTable = js.native
+      @JSGlobal("aleastchs.facade.google.QueryResponse")
+      @js.native
+      class QueryResponse extends js.Object {
 
-    def drawChart(chart_JSON_or_object: js.Any): Unit = js.native
+      }
 
-    def drawToolbar(): Unit = js.native
+      @JSGlobal("aleastchs.facade.google.visualization")
+      @js.native
+      object visualization extends js.Object {
+        // STATIC METHODS
+        def arrayToDataTable(twoDArray: js.Array[js.Array[js.Any]], opt_firstRowIsData: Boolean = false): DataTable = js.native
 
+        def drawChart(chart_JSON_or_object: js.Any): Unit = js.native
 
-    @JSName("google.visualization.AnnotationChart")
-    @js.native
-    class AnnotationChart(container: js.Dynamic) extends js.Object {
-
-    }
-
-    def arrayToDataTable(): Unit = js.native
+        def drawToolbar(): Unit = js.native
 
 
-    @JSName("google.visualization.ColumnChart")
-    @js.native
-    class ColumnChart(container: js.Dynamic) extends js.Object {
+        @JSName("aleastchs.facade.google.visualization.AnnotationChart")
+        @js.native
+        class AnnotationChart(container: js.Dynamic) extends js.Object {
 
-    }
+        }
 
-
-    @JSName("google.visualization.Timeline")
-    @js.native
-    class Timeline(container: js.Dynamic) extends js.Object  {
-    }
-
-    @JSName("google.visualization.DataTable")
-    @js.native
-    class DataTable(
-                     optional_data: String = "",
-                     optional_version: String = "") extends js.Object {
-      def addColumn(`type`: String, opt_label: String = "", opt_id: String = ""): Int = js.native
-
-      def addColumn(description_object: Array[String]): Int = js.native
-
-      def addRows(rows: js.Array[js.Array[js.Any]]): Int = js.native
-
-      def addRows(numOrArray: js.Any): Int = js.native
-
-      def addRow(row: js.Array[js.Any]): Int = js.native
-
-      def addRow(): Int = js.native
-
-      def getColumnId(columnIndex: Int): String = js.native
-
-      def getColumnLabel(columnIndex: Int): String = js.native
-
-      def getColumnPattern(columnIndex: Int): String = js.native
-
-      def getColumnProperties(columnIndex: Int): js.Object = js.native
-
-      def getColumnProperty(columnIndex: Int, name: String): js.Any = js.native
-
-      def getColumnRange(columnIndex: Int): js.Object = js.native
-
-      def getColumnRole(columnIndex: Int): String = js.native
-
-      def getColumnType(columnIndex: Int): String = js.native
-
-      def getDistinctValues(columnIndex: Int): js.Array[js.Object] = js.native
-
-      def getFilteredRows(filters: js.Array[js.Object]): js.Array[js.Object] = js.native
-
-      def getFormattedValue(rowIndex: Int, columnIndex: Int): String = js.native
-
-      def getNumberOfColumns(): Int = js.native
-
-      def getNumberOfRows(): Int = js.native
-
-      def getProperties(rowIndex: Int, columnIndex: Int): js.Object = js.native
-
-      def getTableProperties(): js.Object = js.native
-
-      def toJSON(): String = js.native
-
-      override def clone(): DataTable = js.native
-    }
-
-    @JSName("google.visualization.events")
-    @js.native
-    class events extends js.Object {
-      // todo Fix source_visualization and handling_function
-      // todo fix result value
-      //def addListener(source_visualization: _, event_name: String, handling_function: _): Unit = js.native
-      //def addOneTimeListener(source_visualization: _, event_name: String, handling_function: _): Unit = js.native
-      //def removeListener(listener_handler: _): Unit = js.native
-      //def removeAllListeners(source_visualization: _): Unit = js.native
-      //def trigger(source_visualization: _, event_name: String, event_args: _): Unit = js.native
-    }
+        def arrayToDataTable(): Unit = js.native
 
 
-    @JSName("google.visualization.errors")
-    @js.native
-    object errors extends js.Object {
-      def addError(container: js.Dynamic, message: String, opt_detailedMessage: String, opt_options: js.Object): Unit = js.native
+        @JSName("aleastchs.facade.google.visualization.ColumnChart")
+        @js.native
+        class ColumnChart(container: js.Dynamic) extends js.Object {
 
-      // todo fix response value
-      def addErrorFromQueryResponse(container: js.Dynamic, response: js.Any): Unit = js.native
+        }
+
+
+        @JSName("aleastchs.facade.google.visualization.Timeline")
+        @js.native
+        class Timeline(container: js.Dynamic) extends js.Object {
+          def draw(data: DataTable, options: js.Object): Unit = js.native
+
+          def clearChart(): Unit = js.native
+
+          def getSelection(): js.Array[js.Any] = js.native
+
+        }
+
+        @JSName("aleastchs.facade.google.visualization.DataTable")
+        @js.native
+        class DataTable(
+                         optional_data: String = "",
+                         optional_version: String = "") extends js.Object {
+          def addColumn(`type`: String, opt_label: String = "", opt_id: String = ""): Int = js.native
+
+          def addColumn(description_object: Array[String]): Int = js.native
+
+          def addRows(rows: js.Array[js.Array[js.Any]]): Int = js.native
+
+          def addRows(numOrArray: js.Any): Int = js.native
+
+          def addRow(row: js.Array[js.Any]): Int = js.native
+
+          def addRow(): Int = js.native
+
+          def getColumnId(columnIndex: Int): String = js.native
+
+          def getColumnLabel(columnIndex: Int): String = js.native
+
+          def getColumnPattern(columnIndex: Int): String = js.native
+
+          def getColumnProperties(columnIndex: Int): js.Object = js.native
+
+          def getColumnProperty(columnIndex: Int, name: String): js.Any = js.native
+
+          def getColumnRange(columnIndex: Int): js.Object = js.native
+
+          def getColumnRole(columnIndex: Int): String = js.native
+
+          def getColumnType(columnIndex: Int): String = js.native
+
+          def getDistinctValues(columnIndex: Int): js.Array[js.Object] = js.native
+
+          def getFilteredRows(filters: js.Array[js.Object]): js.Array[js.Object] = js.native
+
+          def getFormattedValue(rowIndex: Int, columnIndex: Int): String = js.native
+
+          def getNumberOfColumns(): Int = js.native
+
+          def getNumberOfRows(): Int = js.native
+
+          def getProperties(rowIndex: Int, columnIndex: Int): js.Object = js.native
+
+          def getTableProperties(): js.Object = js.native
+
+          def removeRows(rowIndex: Int, numberOfRows: Int): Unit = js.native
+
+          def setCell(rowIndex: Int, columnIndex: Int): Unit = js.native
+
+          def setCell(rowIndex: Int, columnIndex: Int, value: String): Unit = js.native
+
+          def setCell(rowIndex: Int, columnIndex: Int, value: String = null, formattedValue: String = null, properties: js.Object = null): Unit = js.native
+
+          def setColumnLabel(columnIndex: Int, label: String): Unit = js.native
+
+          def setColumnProperty(columnIndex: Int, name: String, value: String): Unit = js.native
+
+          def setColumnProperties(columnIndex: Int, properties: js.Object): Unit = js.native
+
+          def setFormattedValue(rowIndex: Int, columnIndex: Int, formattedValue: String): Unit = js.native
+
+          def setProperty(rowIndex: Int, columnIndex: Int, name: String, value: String): Unit = js.native
+
+          def setProperties(rowIndex: Int, columnIndex: Int, properties: js.Object): Unit = js.native
+
+          def setRowProperty(rowIndex: Int, name: String, value: String): Unit = js.native
+
+          def setRowProperties(rowIndex: Int, properties: js.Object): Unit = js.native
+
+          def setTableProperty(name: String, value: String): Unit = js.native
+
+          def setTableProperties(properties: js.Object): Unit = js.native
+
+          def setValue(rowIndex: Int, columnIndex: Int, value: String): Unit = js.native
+
+          def sort(singleNumber: Int): Unit = js.native
+
+          def sort(singleObject: js.Object): Unit = js.native
+
+          def sort(numberOrObjectArray: js.Array[js.Any]): Unit = js.native
+
+          def toJSON(): String = js.native
+
+          override def clone(): DataTable = js.native
+        }
+
+        @JSName("aleastchs.facade.google.visualization.events")
+        @js.native
+        class events extends js.Object {
+          // todo Fix source_visualization and handling_function
+          // todo fix result value
+          //def addListener(source_visualization: _, event_name: String, handling_function: _): Unit = js.native
+          //def addOneTimeListener(source_visualization: _, event_name: String, handling_function: _): Unit = js.native
+          //def removeListener(listener_handler: _): Unit = js.native
+          //def removeAllListeners(source_visualization: _): Unit = js.native
+          //def trigger(source_visualization: _, event_name: String, event_args: _): Unit = js.native
+        }
+
+
+        @JSName("aleastchs.facade.google.visualization.errors")
+        @js.native
+        object errors extends js.Object {
+          def addError(container: js.Dynamic, message: String, opt_detailedMessage: String, opt_options: js.Object): Unit = js.native
+
+          // todo fix response value
+          def addErrorFromQueryResponse(container: js.Dynamic, response: js.Any): Unit = js.native
+
+        }
+
+      }
 
     }
 
