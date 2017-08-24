9fb120035eeb1a372acc5e1d3c4440268e8c724b
aleastChs <aleast@student.chalmers.se>
2017-08-24 12:51:04 +0200

    Moved charts-code to object

---
+++ src/main/scala/aleastchs/googleCharts/google/Charts.scala
@@ -0,0 +1,20 @@
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+/**
+  * Created by alexa on 24/08/2017.
+  */
+
+@js.native
+@JSGlobal("google.charts")
+object Charts extends js.Object {
+  def load(): Unit = js.native
+  def load(loading: String): Unit = js.native
+  def load(loading: String, loadObject: js.Object): Unit = js.native
+
+  def setOnLoadCallback(): Unit = js.native
+  def setOnLoadCallback(callback: Unit): Unit = js.native
+  def setOnLoadCallback(callback: js.Function): Unit = js.native
+  def setOnLoadCallback(callback: js.Function0[Unit]): Unit = js.native
+  def setOnLoadCallback(callback: js.Function1[Unit, Unit]): Unit = js.native
+}
